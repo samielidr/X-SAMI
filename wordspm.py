267c79e6efad8442dfecf631447abec8d0bea516
samielidr <92677752+samielidr@users.noreply.github.com>
2021-10-17 14:25:58 +0100

    wordspm.py

---
+++ wordspm.py
@@ -0,0 +1,16 @@
+from termcolor import colored
+
+print(colored("                                    WORD SPAMMER BY X-SAMI",'yellow'))
+
+word = input ("the word:    ")
+amount = int (input('How much time:  ', ))
+if word == ("sami"):
+    print ('hey admin, welcome back')
+
+if word != ("sami"):
+    for i in range(amount):
+        print(word)
+
+
+print (colored('                    script by Xsami, Thanks for using!', 'green'))
+
